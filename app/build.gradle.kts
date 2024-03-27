90e7f3afaf455674ef1874e959f0701e9d2e3bf1
ultimatecompbg <baystancho0@gmail.com>
2024-03-27 23:47:30 +0200

    Recipe adapter and layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
 
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("androidx.recyclerview:recyclerview:1.3.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
---
+++ app/src/main/java/com/vikves/receptosearch/adapter/RecipeListAdapter.java
@@ -0,0 +1,67 @@
+package com.vikves.receptosearch.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.vikves.receptosearch.R;
+import com.vikves.receptosearch.model.Recipe;
+
+import java.util.List;
+
+public class RecipeListAdapter extends RecyclerView.Adapter<RecipeListAdapter.RecipeViewHolder> {
+    private List<Recipe> recipes;
+    private OnRecipeClickListener listener;
+
+    public RecipeListAdapter(List<Recipe> recipes, OnRecipeClickListener listener) {
+        this.recipes = recipes;
+        this.listener = listener;
+    }
+
+    public static class RecipeViewHolder extends RecyclerView.ViewHolder {
+        private TextView titleTextView;
+        private ImageView thumbnailImageView;
+
+        public RecipeViewHolder(View itemView) {
+            super(itemView);
+            titleTextView = itemView.findViewById(R.id.titleTextView);
+            thumbnailImageView = itemView.findViewById(R.id.thumbnailImageView);
+        }
+
+        public void bind(final Recipe recipe, final OnRecipeClickListener listener) {
+            titleTextView.setText(recipe.getTitle());
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    listener.onRecipeClick(recipe);
+                }
+            });
+        }
+    }
+
+    @NonNull
+    @Override
+    public RecipeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recipe_item_layout, parent, false);
+        return new RecipeViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecipeViewHolder holder, int position) {
+        Recipe recipe = recipes.get(position);
+        holder.bind(recipe, listener);
+    }
+
+    @Override
+    public int getItemCount() {
+        return recipes.size();
+    }
+
+    public interface OnRecipeClickListener {
+        void onRecipeClick(Recipe recipe);
+    }
+}
